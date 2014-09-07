918f900f5b526a1389adeb80183a6a395edbd399
David Bolin <davidbolin@gmail.com>
2014-09-07 10:47:17 +0200

    Bug fix to index helper function, add inla marginal helper function, and update excursions.inla

--- R/inlautils.R
+++ R/inlautils.R
@@ -27,10 +27,60 @@ inla.output.indices = function(result, name=NULL, stack=NULL, tag=NULL, ...)
     if (!is.null(name)) {
         nameindex <- which(result$misc$configs$contents$tag == name)
         index <- (result$misc$configs$contents$start[nameindex] - 1L +
-                  seq_len(result$misc$configs$contents$lengthstart[nameindex]))
+                  seq_len(result$misc$configs$contents$length[nameindex]))
     } else { ## Have tag
-        index <- which(stack$data$index[[tag]])
+        index <- stack$data$index[[tag]]
     }
 
     index
 }
+
+
+private.link.function <- function(x, link, inv=FALSE)
+{
+  if (is.na(link)) {
+    link = "identity"
+  }
+  return(do.call(paste("inla.link.", link, sep=""),list(x=x, inv=inv)))
+}
+
+private.get.config <- function(result,i)
+{
+  mu=result$misc$configs$config[[i]]$mean
+	Q=forceSymmetric(result$misc$configs$config[[i]]$Q)
+	vars = diag(result$misc$configs$config[[i]]$Qinv)
+	lp = result$misc$configs$config[[i]]$log.posterior
+  return(list(mu=mu,Q=Q,vars=vars,lp=lp))
+}
+
+## Calculate the marginal probability for X_i>u or X_i<u. 
+## Note that the index 'i' refers to a location in the linear 
+## predictor if predictor==TRUE, whereas it refers to a location 
+## in the random effect vector otherwise.
+inla.get.marginal <- function(i, u,result,effect.name=NULL, u.link, type)
+{ 
+  link = result$misc$linkfunctions$names[result$misc$linkfunctions$link][i]
+
+  if(is.null(effect.name) && u.link == TRUE){
+    #calculate marginals using fitted values 
+    if(link=="identity" || is.na(link)){
+	    marg.p = result$marginals.fitted.values[[i]]
+	  } else {
+	    marg.p = INLA::inla.tmarginal(function(x)
+                              private.link.function(x,link,inv=TRUE),
+                                  result$marginals.fitted.values[[i]])
+	  }
+	} else if (is.null(effect.name)){
+      # Calculate marginals using linear predictor
+      marg.p = result$marginals.linear.predictor[[i]]
+  } else {
+      # Calculate marginals using a random effect
+      marg.p = result$marginals.random[[effect.name]][[i]]
+  }
+    
+	  if(type=='<'){
+		  return(INLA::inla.pmarginal(u,marg.p))
+	  } else {
+		  return(1-INLA::inla.pmarginal(u,marg.p))
+	  }
+  }
