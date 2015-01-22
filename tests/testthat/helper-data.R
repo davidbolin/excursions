8c2d45ccb12d0d2a0258ef7e8d3e4267b05833e2
David Bolin <davidbolin@gmail.com>
2015-01-22 16:43:52 +0100

    Updates to documentation and tests

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -15,6 +15,7 @@ integration.testdata1 <- function()
 
 testdata.inla <- function()
 {
+  if(require(INLA)){
   rho = 0.9
   tau = 15
   n = 11
@@ -22,8 +23,8 @@ testdata.inla <- function()
   x = 1:n
   mu = 10*((x<n/2)*(x-n/2) + (x>=n/2)*(n/2-x)+n/4)/n
   Q = tau*sparseMatrix(i=c(1:n, 2:n), j=c(1:n, 1:(n-1)),
-                     x=c(1,rep(1+rho^2, n-2),1, rep(-rho, n-1)),
-                     dims=c(n, n), symmetric=TRUE)
+                       x=c(1,rep(1+rho^2, n-2),1, rep(-rho, n-1)),
+                       dims=c(n, n), symmetric=TRUE)
   X = mu+solve(chol(Q), rnorm(n))
 
   obs.loc = sample(1:n,n.obs)
@@ -31,17 +32,18 @@ testdata.inla <- function()
   Y = as.vector(A %*% X + rnorm(n.obs))
 
   ef = list(c(list(ar=x),list(cov=mu)))
-  s.obs = inla.stack(data=list(y=Y), A=list(A), effects=ef, tag="obs")
-  s.pre = inla.stack(data=list(y=NA), A=list(1), effects=ef,tag="pred")
-  stack = inla.stack(s.obs,s.pre)
+  s.obs = INLA::inla.stack(data=list(y=Y), A=list(A), effects=ef, tag="obs")
+  s.pre = INLA::inla.stack(data=list(y=NA), A=list(1), effects=ef,tag="pred")
+  stack = INLA::inla.stack(s.obs,s.pre)
   formula = y ~ -1 + cov + f(ar,model="ar1")
-  result = inla(formula=formula, family="normal",
-                data = inla.stack.data(stack),
-                control.predictor=list(A=inla.stack.A(stack),compute=TRUE),
-                control.compute = list(config = TRUE))
+  result = INLA::inla(formula=formula,
+              data = INLA::inla.stack.data(stack),
+              control.predictor=list(A=INLA::inla.stack.A(stack),compute=TRUE),
+              control.compute = list(config = TRUE))
 
   seed = c(1750459768, 1598840523, 249795150,
            2124039812, 1116456203, 1714779982)
   return(list(result=result,stack=stack,seed=seed,n=n))
+  }
 }
 
