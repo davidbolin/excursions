599c950bf77c1787f85a86ae89e68c5d50c5cd6f
Finn Lindgren <finn.lindgren@gmail.com>
2018-07-16 11:25:22 +0100

    Fix error in private.get.config

--- R/inlautils.R
+++ R/inlautils.R
@@ -65,10 +65,11 @@ private.link.function <- function(x, link, inv=FALSE)
 private.get.config <- function(result,i)
 {
   mu=result$misc$configs$config[[i]]$mean
-	Q=forceSymmetric(result$misc$configs$config[[i]]$Q)
-	vars = diag(result$misc$configs$config[[i]]$Qinv)
-	m <- max(unlist(lapply(data$result$misc$configs$config,function(x) x$log.posterior)))
-	lp = result$misc$configs$config[[i]]$log.posterior -m
+  Q=forceSymmetric(result$misc$configs$config[[i]]$Q)
+  vars = diag(result$misc$configs$config[[i]]$Qinv)
+  m <- max(unlist(lapply(result$misc$configs$config,
+                         function(x) x$log.posterior)))
+  lp = result$misc$configs$config[[i]]$log.posterior -m
   return(list(mu=mu,Q=Q,vars=vars,lp=lp))
 }
 
